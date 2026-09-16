52282ff0f1ca515150206301b5741364731d87b3
srng55 <sarangputhiyadath@gmail.com>
2026-09-16 21:00:56 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+
+        for i in range(amount + 1):
+
+            for coin in coins:
+                if i - coin >=0:
+                    dp[i] = min(dp[i], 1 + dp[i-coin])
+        
+        if dp[amount]==float('inf'):
+            return -1
+
+        return dp[amount]
+            
