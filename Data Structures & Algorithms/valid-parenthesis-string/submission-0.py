c2d60da80070db58eb6070331319d6546192fc0d
srng55 <sarangputhiyadath@gmail.com>
2026-09-18 21:52:20 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        low = 0
+        high = 0
+
+        for ch in s:
+
+            if ch == '(':
+                low += 1
+                high += 1
+
+            elif ch == ')':
+                low -=1
+                high -=1
+
+            else:
+                low -=1
+                high +=1
+
+            if high < 0:
+                return False
+
+            low = max(low, 0)
+
+        return low == 0
+        
